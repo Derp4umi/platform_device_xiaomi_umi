4e17f56906e8931c5b10f9388a22e3d4ca5eb0ed
ianchb <i@ap.icu>
2024-01-31 09:52:29 +0800

    Revert "umi: Time for tequila!"
    This reverts commit 3a4d82421be15ac7b8dd3012d917d44684ffeb94.

--- device.mk
+++ device.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-tequila
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
